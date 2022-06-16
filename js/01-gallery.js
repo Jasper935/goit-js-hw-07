a692307110bfcbb34450df786efa0c1a805daa94
Jasper935 <pikunivan@gmail.com>
2022-06-16 21:32:15 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// import * as basicLightbox from 'basiclightbox'
+
+
+
+
+
+const gallery = document.querySelector('.gallery')
+
+const res = galleryItems.reduce((acc, el) =>
+    acc += `<div class="gallery__item">
+<a class="gallery__link" href="${el.original}">
+  <img
+    class="gallery__image"
+    src="${el.preview}"
+    data-source="${el.original}"
+    alt="${el.description}"
+  />
+</a>
+</div>`
+    , '')
+
+gallery.insertAdjacentHTML('afterbegin', res)
+
+
+gallery.addEventListener('click', handlerGallery)
+
+let instance;
+
+function handlerGallery(event) {
+    event.preventDefault()
+
+    if (event.target.className !== 'gallery') {
+
+        instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" />
+      `)
+        instance.show()
+        window.addEventListener('keydown', handlerKey)
+
+    }
+  
+
+}
+
+
+function handlerKey(event) {
+    if (event.key === 'Escape') {
+        instance.close()
+    }
+    window.removeEventListener('keydown', handlerKey)
+
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery')
+
+const res = galleryItems.reduce((acc, el) =>
+    acc += ` <a class="gallery__item" href="${el.original}">
+    <img class="gallery__image" data-source="${el.original}" src="${el.preview}" alt="${el.description}" />
+  </a>`
+    , '')
+   
+
+gallery.insertAdjacentHTML('afterbegin', res)
+
+
+
+ new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250 
+ });
+
+
+
+
+
+
+
+
